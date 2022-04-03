55356da8a4c43e883cecac2dc546e10177e77ad2
andrelimaiftm <andrelima@iftm.edu.br>
2022-04-03 14:31:29 -0300

    16.11. Tratando respostas com código de erro no client Java

--- src/main/java/com/algaworks/algafood/client/ListagemRestauranteMain.java
+++ src/main/java/com/algaworks/algafood/client/ListagemRestauranteMain.java
@@ -2,22 +2,34 @@ package com.algaworks.algafood.client;
 
 import org.springframework.web.client.RestTemplate;
 
+import com.algaworks.algafood.client.api.ClientApiException;
 import com.algaworks.algafood.client.api.RestauranteClient;
 
 public class ListagemRestauranteMain {
 	
-	public static void main(String[] args) {
-		
-		RestTemplate restTemplate = new RestTemplate();
-		
-		String url = "http://localhost:8080";
-		
-		
-		RestauranteClient restauranteClient = 
-				new RestauranteClient(restTemplate, url);
-		
-		restauranteClient.listar().stream()
+	public static void main(String[] args) {		
+		try {
+			
+			RestTemplate restTemplate = new RestTemplate();
+			
+			String url = "http://localhost:8080";			
+			
+			RestauranteClient restauranteClient = 
+					new RestauranteClient(restTemplate, url);
+			
+			restauranteClient.listar().stream()
 			.forEach(restaurante -> System.out.println(restaurante));
+			
+		} catch (ClientApiException e) {
+			if(e.getProblem() != null) {
+				System.out.println(e.getProblem());
+				System.out.println(e.getProblem().getUserMessage());				
+			}else {
+				System.out.println("Erro desconhecido");
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
